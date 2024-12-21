ac753b3cebd5c151960fe6a12421087447338053
cheesestringer <4462478+cheesestringer@users.noreply.github.com>
2024-12-21 20:58:58 +1100

    Create microverse host and home remote
    npx nx g @nx/react:host apps/microverse --remotes=home

---
+++ apps/microverse/src/app/app.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import NxWelcome from './nx-welcome';
+import { Link, Route, Routes } from 'react-router-dom';
+
+const Home = React.lazy(() => import('home/Module'));
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Microverse</Link>
+        </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="microverse" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </React.Suspense>
+  );
+}
+
+export default App;
